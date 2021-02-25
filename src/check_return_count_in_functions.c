3a452aad80b7bf479ebde7363018a1257a9a808a
kokke <spam@rowdy.dk>
2021-02-25 13:10:53 +0100

    Update check_return_count_in_functions.c

--- src/check_return_count_in_functions.c
+++ src/check_return_count_in_functions.c
@@ -3,7 +3,6 @@
 #include <string.h> /* strncmp */
 
 
-static int paren_lvl = 0;    /* balance of '(', ')' */
 static int brace_lvl = 0;    /* balance of '{', '}' */
 static int state = 0;
 static int state_last_warn = 0;
@@ -12,7 +11,6 @@ static int state_last_warn = 0;
 /* reset balance counter when loading new file */
 void check_return_count_in_functions_init(void)
 {
-  paren_lvl = 0;
   brace_lvl = 0;
   state = 0;
 }
@@ -45,11 +43,7 @@ void check_return_count_in_functions_new_token(struct source_file* s, struct tok
 
        if (toks[i].toktyp == OP_LBRACE) { brace_lvl += 1; }
   else if (toks[i].toktyp == OP_RBRACE) { brace_lvl -= 1; }
-  else if (toks[i].toktyp == OP_LPAREN) { paren_lvl += 1; }
-  else if (toks[i].toktyp == OP_RPAREN) { paren_lvl -= 1; }
 
-  if (paren_lvl < 0) { paren_lvl = 0; }
   if (brace_lvl < 0) { brace_lvl = 0; }
-
 }
 
