ca4f37f16dcadcf44ec747f9c2c1ae72b769507d
kokke <spam@rowdy.dk>
2021-02-25 14:09:06 +0100

    Update test_return_count_in_functions.c

--- tests/test_return_count_in_functions.c
+++ tests/test_return_count_in_functions.c
@@ -10,10 +10,33 @@ static int function1(int x)
   if (x == 7) return 7; /* HIT */
 }
 
+static int function2(void)
+{
+  if (rand() == 0) return 0;
+  if (rand() == 1) return 1;
+  if (rand() == 2) return 2;
+  if (rand() == 3) return 3; /* HIT */
+  if (rand() == 4) return 4; /* HIT */
+  if (rand() == 5) return 5; /* HIT */
+  if (rand() == 6) return 6; /* HIT */
+}
+
+static int function3(int x)
+{
+  if (x & 1) return 0;
+  else return 1;
+}
+
+
+
 
 int main(void)
 {
-  return function1(rand());
+  int x = function1(rand());
+  function2();
+  int y = function3(x);
+
+  return 0;
 }
 
 
